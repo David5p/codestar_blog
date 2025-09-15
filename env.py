b329506f936ae8e8734d67331ee0e9e1b35db667
David5p <k9glpuwd@students.codeinstitute.net>
2025-09-15 13:42:37 +0200

    Update settings and env files

--- env.py
+++ env.py
@@ -1,4 +1,7 @@
 import os
 
+os.environ.setdefault('SECRET_KEY', 's3cr3tK3y!$#@123_AbCxyz456')
 os.environ.setdefault(
-    "DATABASE_URL", "postgresql://neondb_owner:npg_vD3oiPNM2Ysa@ep-summer-union-agx47k7w.c-2.eu-central-1.aws.neon.tech/gray_same_deal_220695")
+    "DATABASE_URL",
+    "postgresql://neondb_owner:npg_vD3oiPNM2Ysa@ep-summer-union-agx47k7w.c-2.eu-central-1.aws.neon.tech/gray_same_deal_220695"
+)
