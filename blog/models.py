955740e21fcb0291ceb0e301cc01115cbcf31f1c
David5p <k9glpuwd@students.codeinstitute.net>
2025-09-19 14:13:47 +0200

    fix error

--- blog/models.py
+++ blog/models.py
@@ -1,11 +1,12 @@
 from django.db import models
-from django.contrib.auth.models import User
+from django.contrib.auth.models import User # Import models to connect
 
 STATUS = ((0, "Draft"), (1, "Published"))
+
 # Create your models here.
 class Post(models.Model):
     title = models.CharField(max_length=200, unique=True)
-    slug = models.SlugField(max_length=200, unique=True, default="temp-slug")
+    slug = models.SlugField(max_length=200, unique=True)
     author = models.ForeignKey(
         User, on_delete=models.CASCADE, related_name="blog_posts"
     )
@@ -14,10 +15,13 @@ class Post(models.Model):
     status = models.IntegerField(choices=STATUS, default=0)
     excerpt = models.TextField(blank=True)
     updated_on = models.DateTimeField(auto_now=True)
+
     class Meta:
         ordering = ["-created_on"]
+    
     def __str__(self):
-        return f"The title of this post is {self.title} | written by {self.author}"
+        return f"{self.title} | written by {self.author}"
+
 
 class Comment(models.Model):
     post = models.ForeignKey(
@@ -27,7 +31,11 @@ class Comment(models.Model):
     body = models.TextField()
     approved = models.BooleanField(default=False)
     created_on = models.DateTimeField(auto_now_add=True)
+    new_field = models.SlugField()
+
     class Meta:
         ordering = ["created_on"]
+
     def __str__(self):
-        return f"Comment {self.body} | by {self.author}"
+        return f"Comment {self.body} by {self.author}"
+        
