e27ef66be9e20f2ee2b33f6116b4ae0271edb125
David5p <k9glpuwd@students.codeinstitute.net>
2025-09-22 14:44:56 +0200

    Fix static files with Whitenoise and collectstatic

--- codestar/settings.py
+++ codestar/settings.py
@@ -136,6 +136,10 @@ STATIC_URL = 'static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static'), ]
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
+# Static files storage for Whitenoise (Heroku)
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
