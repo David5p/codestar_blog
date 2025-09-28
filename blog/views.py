7287e54aca040a970e6c4ce240ecab941448dac8
David5p <k9glpuwd@students.codeinstitute.net>
2025-09-28 14:48:28 +0200

    Add collaborate form

--- blog/views.py
+++ blog/views.py
@@ -1,34 +1,24 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 from django.views import generic
 from django.contrib import messages
 from .models import Post
 from .forms import CommentForm
+from django.contrib.auth.models import User
 
-# Create your views here.
+# CLASS-BASED VIEW FOR POST LIST
 class PostList(generic.ListView):
     queryset = Post.objects.filter(status=1)
     template_name = "blog/index.html"
     paginate_by = 6
 
+# FUNCTION-BASED VIEW FOR POST DETAIL AND COMMENT HANDLING
 def post_detail(request, slug):
-    """
-    Display an individual :model:`blog.Post`.
+    post = get_object_or_404(Post, slug=slug, status=1)
+    comments = post.comments.filter(approved=True).order_by('-created_on')
+    comment_count = comments.count()
 
-    **Context**
-
-    ``post``
-        An instance of :model:`blog.Post`.
-
-    **Template:**
-
-    :template:`blog/post_detail.html`
-    """
-
-    queryset = Post.objects.filter(status=1)
-    post = get_object_or_404(queryset, slug=slug)
-    comments = post.comments.all().order_by("-created_on")
-    comment_count = post.comments.filter(approved=True).count()
     if request.method == "POST":
+        print("Received a POST request")
         comment_form = CommentForm(data=request.POST)
         if comment_form.is_valid():
             comment = comment_form.save(commit=False)
@@ -38,22 +28,26 @@ def post_detail(request, slug):
             messages.add_message(
                 request, messages.SUCCESS,
                 'Comment submitted and awaiting approval'
-    )
-    comment_form = CommentForm()
+            )
+            return redirect('post_detail', slug=post.slug)
+    else:
+        comment_form = CommentForm()
+
+    print("About to render template")
 
     return render(
         request,
         "blog/post_detail.html",
-        {"post": post, 
-        "comments": comments,
-        "comment_count": comment_count,
-        "comment_form": comment_form,
-         },
-
+        {
+            "post": post,
+            "comments": comments,
+            "comment_count": comment_count,
+            "comment_form": comment_form,
+        },
     )
 
+# FUNCTION-BASED VIEW FOR EVENTS
 def event_detail(request, event_id):
-    
     queryset = Event.objects.all()
     event = get_object_or_404(queryset, event_id=event_id)
 
