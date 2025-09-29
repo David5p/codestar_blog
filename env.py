69c63eb9620764730f6c9b76b032f3b34d8ba4cf
David5p <k9glpuwd@students.codeinstitute.net>
2025-09-29 17:33:21 +0200

    Add cloudinary

--- env.py
+++ env.py
@@ -5,3 +5,8 @@ os.environ.setdefault(
     "DATABASE_URL",
     "postgresql://neondb_owner:npg_vD3oiPNM2Ysa@ep-summer-union-agx47k7w.c-2.eu-central-1.aws.neon.tech/gray_same_deal_220695"
 )
+import os
+
+os.environ.setdefault(
+    "CLOUDINARY_URL", "cloudinary://654375246163865:Wr6tKeEOUB-PwbpaxdMp8n-p8Bo@dsqjcr7ml"
+)
