695e4ea9e8c2a465ac59d1992ca09ef2bb20bb7b
David5p <k9glpuwd@students.codeinstitute.net>
2025-09-30 15:13:03 +0200

    Add about profile image

--- codestar/settings.py
+++ codestar/settings.py
@@ -167,6 +167,9 @@ STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
+
+DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
+
 LOGGING = {
     'version': 1,
     'disable_existing_loggers': False,
